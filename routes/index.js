0e50feb9015388d5979fbcb2804256637ac658e7
Isidora <korac.isidora@gmail.com>
2020-07-04 14:23:05 +0200

    First code

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+// Login/Landing page
+// Get /
+router.get("/", (req, res) => {
+  res.render("login");
+});
+
+// Dashboard page
+// Get /dashboard
+router.get("/dashboard", (req, res) => {
+    res.render("dashboard");
+  });
+
+module.exports = router;

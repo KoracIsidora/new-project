d1b15b6dd964df94951844fc4f9e961f3ea1135d
Isidora <korac.isidora@gmail.com>
2020-07-07 14:04:18 +0200

    Finished project on nodejs

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const morgan = require("morgan");
 const exphbs = require("express-handlebars");
 const passport = require("passport");
 const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 const connectDB = require("./config/db");
 
 //Load config
@@ -19,12 +22,33 @@ connectDB();
 
 const app = express();
 
+// Body parser
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+
+// Metfo override
+app.use(methodOverride(function (req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    let method = req.body._method
+    delete req.body._method
+    return method
+  }
+}))
+
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+// Helpers
+const {formatDate, stripTags, truncate, editIcon, select} = require('./helpers/hbs')
+
 // Handlebars
 app.engine('.hbs', exphbs({
+  helpers: {
+    formatDate, stripTags, truncate, editIcon, select
+  },
   defaultLayout: 'main',
   extname: '.hbs'
 }));
@@ -34,12 +58,19 @@ app.set('view engine', '.hbs');
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
-  saveUninitialized: false
+  saveUninitialized: false,
+  store: new MongoStore({mongooseConnection: mongoose.connection })
 }))
 
 // Passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
+
+// Set global variable
+app.use(function (req, res, next) {
+  res.locals.user = req.user || null;
+  next();
+})
 
 // Static folder
 app.use(express.static(path.join(__dirname, "public")))
@@ -47,6 +78,7 @@ app.use(express.static(path.join(__dirname, "public")))
 // Routes
 app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
 
 const PORT = process.env.PORT || 3000;
 
